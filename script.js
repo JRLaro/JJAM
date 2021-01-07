85f69a09965e22fefda42122cd7b68f94837aafc
J.Laro <joselarojr@gmail.com>
2021-01-07 11:22:58 -0500

    inital commit, created the HTML, CSS, JS folders, added the drink API

---
+++ script.js
@@ -0,0 +1,15 @@
+// drink API
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://the-cocktail-db.p.rapidapi.com/filter.php?i=Gin",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "6eeca3cb10mshb5576aa4173f1ccp188733jsn2d1f211f3f82",
+		"x-rapidapi-host": "the-cocktail-db.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
