88116d94b52260947ec7b1ae2a63725ed872650b
J.Laro <joselarojr@gmail.com>
2021-01-10 19:38:12 -0500

    created user login popUp. JS - needs to take user input and append on nav-bar.

--- script.js
+++ script.js
@@ -1,7 +1,47 @@
 $(document).ready(function () {
 
+    // const loginPopup = $(".login-popup");
+    const loginPopup = document.querySelector(".login-popup");
 
-//let the javascript BEGIIIIN
+    // const close = $(".close");
+
+    const close = document.querySelector(".close");
+    
+    window.addEventListener("load", function () {
+        
+        // showPopup();
+        setTimeout(function () {
+            // $(loginPopup).attr("show");
+            loginPopup.classList.add("show");
+        },5000)
+
+    })
+    
+    // function showPopup() {
+    //     const timeLimit = 5; // this reveals popUp in 5 seconds
+    //     let i = 0;
+    //     const timer = setInterval(function () {
+    //         i++;
+
+    //         if (i == timeLimit) {
+    //             clearInterval(timer);
+    //             loginPopup.classList.add("show");
+    //         }
+    //         console.log(i);
+    //     }, 1000);
+    // }
+
+    close.addEventListener("click", function () {
+        loginPopup.classList.remove("show");
+    })
+
+
+
+
+
+
+
+
 
 
  })
