888c1067c6d2584650c4eb9d2aaf33a572398403
Jordan Masone <jordanmasone1@gmail.com>
2021-01-11 17:42:24 -0500

    Adding updated food files.

---
+++ food.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+	$("#userTextInput").keypress(function (event) {
+		if (event.which === 13) {
+			console.log(event)
+			var text = $("#userTextInput").val();
+			var drinkBtn = $("<button>").addClass("button m-1 is-primary has-text-weight-bold").text(text);
+			var drinkDelete = $("<button>").addClass("delete is-small");
+			drinkBtn.append(drinkDelete);
+			$("#ingredientsCol").append(drinkBtn)
+			$("#userTextInput").val("")
+
+		}
+	})
+
+
+	$("#ingredientsCol").on("click", ".button > .delete", function(){
+		$(this).parent().remove();
+		console.log($(this));
+
+	})
+
+	$("#searchButton").click(function(){
+		$("#mainContainer").show();
+	})
+
+	$("#resetButton").click(function(){
+		$("#ingredientsCol").empty();
+		console.log("this is working")
+		$("#mainContainer").hide()
+	})
+})
